ce032ce665f9b8911bafdf700434788137188214
Ayush Kumar <102464313+ayush9h@users.noreply.github.com>
2023-09-22 21:45:56 +0530

    Create reverseArray.cpp

---
+++ Arrays/reverseArray.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    
+    vector<int>arr(n,0);
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+        
+    int low = 0;
+    int high = n-1; 
+    
+    while(low<=high)
+    {
+        swap(arr[low],arr[high]);
+        low++;
+        high--;
+    }
+    
+    
+     for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+        
+    return 0;
+}
