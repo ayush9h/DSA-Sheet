a4072c1dab6207352d88874688495bf656385767
Ayush Kumar <102464313+ayush9h@users.noreply.github.com>
2024-03-09 11:39:07 +0530

    Update minCommonVal.cpp

--- BinarySearch/minCommonVal.cpp
+++ BinarySearch/minCommonVal.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//Usin HashMaps
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         map<int,int> mpp1;
         for(int i = 0;i<nums1.size();i++)
@@ -21,5 +22,32 @@ public:
 
         //time - O(N)
         //space - O(N)
+    }
+};
+//Using Two Pointers
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        int i = 0, j =0;
+        int ans = -1;
+        while(i < n && j < m)
+        {
+            //sorted property to be followed
+            if(nums1[i]!=nums2[j] && nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else if(nums1[i]!=nums2[j] && nums1[i] > nums2[j])
+            {
+                j++;
+            }
+            else{
+                ans = nums1[i];
+                break;
+            }
+        }
+        return ans;
     }
 };
